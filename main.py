a2768621d6ea635008a444795f87a24a2978cd69
Aria Zaryab <ariazaryab@gmail.com>
2026-01-06 23:24:11 +0330

    ellipse for players

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from utils import read_video, save_video
 from trackers import Tracker
 
 def main():
+    # Read Video
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
+    # Initialize Tracker
     tracker = Tracker('models/best.pt')
 
-    tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/tracks_stubs.pkl')
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/tracks_stubs.pkl')
 
-    save_video(video_frames, 'output_videos/output_video.avi')
+    # Draw Output
+    ## Draw Object Track
+    output_video_frames = tracker.draw_annotation(video_frames, tracks)
+
+    # Save Video
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
 if __name__ == '__main__':
     main()
